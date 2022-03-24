4b89ffa93120c17f6069e5f7301dc71b76c3c924
Naycom <web@naycomagency.it>
2022-03-24 09:55:10 +0100

    Puglia Ricambi App

---
+++ android/app/src/main/kotlin/io/rnlab/cirilla/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pugliaricambi.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/app.dart
@@ -0,0 +1,13 @@
+const String baseUrl = 'https://www.pugliaricambi.it/';
+
+const String consumerKey = 'ck_c9e845bd455bbc859d1ad4cbe0b9c97a5d1df595';
+
+const String consumerSecret = 'cs_93f9018a9cadf53ef798d78afb58c27d14667e7d';
+
+const String restPrefix = 'wp-json';
+
+const String defaultLanguage = 'it';
+
+const List<String> languageSupport = ['en', 'ar', 'tr'];
+
+const String googleClientId = '295269595518-e7s01ueadskq7sbg2k4g4dfnefpmd7vt.apps.googleusercontent.com';
